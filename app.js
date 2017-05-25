9929c144324a10ce6eaf403b2b6a0a4624107e67
Andrew Eason <aeason96@vt.edu>
2017-05-25 16:04:18 -0400

    blocks aren't persistent

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/game.html');
 });
 var allClients = [];
+var clientDivs = [];
 io.on('connection', function(socket){
     console.log("user connected");
     allClients.push(socket);
